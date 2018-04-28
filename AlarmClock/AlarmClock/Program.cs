b75fdfd90bde544582a971908b7fa459512526bb
Matt <12matQ@gmail.com>
2018-04-28 11:51:26 +0200

    update

---
+++ AlarmClock/AlarmClock/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SpotifyAPI.Web; //Base Namespace
+using SpotifyAPI.Web.Auth; //All Authentication-related classes
+using SpotifyAPI.Web.Enums; //Enums
+using SpotifyAPI.Web.Models; //Models for the JSON-responses
+
+namespace AlarmClock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SpotifyWebAPI _spotify = new SpotifyWebAPI()
+            {
+                UseAuth = false,
+            };
+
+            FullTrack track = _spotify.GetTrack("3Hvu1pq89D4R0lyPBoujSv");
+
+            Console.WriteLine(track.Name);
+        }
+    }
+}
